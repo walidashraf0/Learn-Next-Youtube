d09f3644b9a1c067fdaa447b520c8fda22ad0e41
Walid Ashraf <walidashraf050@gmail.com>
2025-12-04 02:18:52 +0200

    Using Axios

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,19 +1,31 @@
+'use client';
 import { TPost } from "@/utils/types";
+import axios from "axios";
+import { use, useEffect, useState } from "react";
 
-const PostPage = async ({params}: {params: Promise<{ id: string }>})  => {
-  const { id } = await params;
-  const res = await fetch(
-    `https://jsonplaceholder.typicode.com/posts/${id}`
-  );
+const PostPage = ({params}: {params: Promise<{ id: string }>})  => {
+  const { id } = use(params);
 
-  const post: TPost = await res.json();
+  const [post, setPost] = useState<TPost | null>(null);
+    const getPost = async () => {
+      try {
+        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
+        setPost(res.data);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+  
+    useEffect(() => {
+      getPost();
+    }, []);
 
   return (
     <div className="p-2 md:w-2/5 bg-gray-400 border-2 border-blue-400 rounded-md">
       <h2 className="text-2xl font-bold text-green-400">
-        {post.title} <span className="text-2xl">{post.id}</span>
+        {post?.title} <span className="text-2xl">{post?.id}</span>
       </h2>
-      <p className="text-sm text-gray-600">{post.body}</p>
+      <p className="text-sm text-gray-600">{post?.body}</p>
     </div>
   );
 };
