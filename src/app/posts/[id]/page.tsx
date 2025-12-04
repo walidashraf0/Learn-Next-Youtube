c4f7cae541f16db85a43869685b323f0153b28c6
Walid Ashraf <walidashraf050@gmail.com>
2025-12-04 19:19:22 +0200

    Search Input & Pagination & Add Comment Form

--- src/app/posts/[id]/page.tsx
+++ src/app/posts/[id]/page.tsx
@@ -1,32 +1,47 @@
-'use client';
+"use client";
+import AddCommentForm from "@/components/comments/AddCommentForm";
+import CommentItem from "@/components/comments/CommentItem";
 import { TPost } from "@/utils/types";
 import axios from "axios";
 import { use, useEffect, useState } from "react";
 
-const PostPage = ({params}: {params: Promise<{ id: string }>})  => {
+const PostPage = ({ params }: { params: Promise<{ id: string }> }) => {
   const { id } = use(params);
 
   const [post, setPost] = useState<TPost | null>(null);
-    const getPost = async () => {
-      try {
-        const res = await axios.get(`https://jsonplaceholder.typicode.com/posts/${id}`);
-        setPost(res.data);
-      } catch (error) {
-        console.log(error);
-      }
-    };
-  
-    useEffect(() => {
-      getPost();
-    }, []);
+  const getPost = async () => {
+    try {
+      const res = await axios.get(
+        `https://jsonplaceholder.typicode.com/posts/${id}`
+      );
+      setPost(res.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getPost();
+  }, []);
 
   return (
-    <div className="p-2 md:w-2/5 bg-gray-400 border-2 border-blue-400 rounded-md">
-      <h2 className="text-2xl font-bold text-green-400">
-        {post?.title} <span className="text-2xl">{post?.id}</span>
-      </h2>
-      <p className="text-sm text-gray-600">{post?.body}</p>
-    </div>
+    <>
+      <div className="container mx-auto">
+        <div className="my-4 mx-auto p-2 w-full md:w-2/3 bg-gray-400 border-2 border-blue-400 rounded-md">
+          <h2 className="text-2xl font-bold text-green-400">
+            {post?.title} <span className="text-2xl">{post?.id}</span>
+          </h2>
+          <p className="text-sm text-gray-600">{post?.body}</p>
+        </div>
+        <AddCommentForm />
+        <CommentItem comment={"HIIIII"} />
+        <CommentItem comment={"HIIIII"} />
+        <CommentItem comment={"HIIIII"} />
+        <CommentItem comment={"HIIIII"} />
+        <CommentItem comment={"HIIIII"} />
+        <CommentItem comment={"HIIIII"} />
+      </div>
+    </>
   );
 };
 
--- src/app/posts/page.tsx
+++ src/app/posts/page.tsx
@@ -1,5 +1,7 @@
 "use client";
+import Pagination from "@/components/Pagination/Pagination";
 import PostItem from "@/components/PostItem/PostItem";
+import SearchPostInput from "@/components/SearchPostInput/SearchPostInput";
 import { TPost } from "@/utils/types";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -7,7 +9,6 @@ import { useEffect, useState } from "react";
 const PostsPage = () => {
   const [posts, setPosts] = useState<TPost[]>([]);
 
-  
   const getPosts = async () => {
     try {
       const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
@@ -23,10 +24,14 @@ const PostsPage = () => {
 
   return (
     <>
-      <div className="flex items-center justify-center flex-wrap gap-7">
-        {posts?.slice(0, 6).map((post) => (
-          <PostItem key={post.id} post={post} />
-        ))}
+      <div className="container m-auto px-4">
+        <SearchPostInput />
+        <div className="flex items-center justify-center flex-wrap gap-7">
+          {posts?.slice(0, 6).map((post) => (
+            <PostItem key={post.id} post={post} />
+          ))}
+        </div>
+        <Pagination />
       </div>
     </>
   );
